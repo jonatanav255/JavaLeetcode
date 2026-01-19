40f7762304d2318e942833898b7e44d74cccb915
Jonatan <jonatanav255@hotmail.com>
2026-01-18 22:36:29 -0600

    missing number

---
+++ Arrays/P268_Missing Number.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+// complexity 
+// time = O(N)
+// space = O(1)
+class P268_MissingNumber {
+
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        //3 * (3 + 1) / 2     
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+        P268_MissingNumber solution = new P268_MissingNumber();
+
+        int[] nums1 = {3, 0, 1};
+
+        System.out.println("Test 1: " + solution.missingNumber(nums1));
+
+    }
+
+}
