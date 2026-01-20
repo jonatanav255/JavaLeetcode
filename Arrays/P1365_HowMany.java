96c94e45cbd9551d9c3d9b9eaabff86031e16d5a
Jonatan <jonatanav255@hotmail.com>
2026-01-20 15:05:14 -0600

    how many 1365

---
+++ Arrays/P1365_HowMany.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class P1365_HowMany {
+
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        P1365_HowMany solution = new P1365_HowMany();
+
+        int[] nums1 = {8, 1, 2, 2, 3};
+        // [8,1,2,2,3]
+        // Output: [4,0,1,1,3]
+
+        System.out.println("Test 1: " + solution.smallerNumbersThanCurrent(nums1));
+
+    }
+}
