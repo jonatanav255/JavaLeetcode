59dabb712cba3f273f460aae69d644dd1d62e936
Jonatan <jonatanav255@hotmail.com>
2026-01-20 01:08:36 -0600

    adding more explanation to it

--- Arrays/P1_TwoSum.java
+++ Arrays/P1_TwoSum.java
@@ -1,16 +1,22 @@
 package Arrays;
 
+import java.lang.management.ClassLoadingMXBean;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+// time complexity: 0(N) because it has to loop the whole array
+// space complexity: 0(N) the Hashmap can store up to N values
 
 public class P1_TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
+        // {2, 7, 11, 15}
+        // 7, 2
+        // target 9
         Map<Integer, Integer> mapElements = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-
+            System.out.println(mapElements);
             if (mapElements.containsKey(complement)) {
                 return new int[]{mapElements.get(complement), i};
             }
@@ -23,7 +29,11 @@ public class P1_TwoSum {
 
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
+        // int[] nums = {3,2,4};
+        // int target = 6;
         int target = 9;
+        // Output: [0,1]
+        // Output: [1,2]
 
         P1_TwoSum solution = new P1_TwoSum();
 
